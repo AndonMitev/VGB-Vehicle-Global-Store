0595be666eaa74b87fb016e2b81096ea2ac2fa33
Andon Mitev <amitev@soft2run.com>
2019-01-26 11:43:51 +0200

    Bootstrap added

--- server/configuration/server.js
+++ server/configuration/server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const config = require('./config');
+const cors = require('cors');
 require('./database')(config.development);
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,13 +12,12 @@ require('./passport')(passport);
 
 const routes = require('./routes');
 app.use(routes);
-
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use((req, res, next) => {
   if (req.user) {
     res.locals.currentUser = req.user;
   }
   next();
 });
-
 module.exports = app.listen(config.development.port,
   () => console.log(`Server is running on port ${config.development.port}`));
